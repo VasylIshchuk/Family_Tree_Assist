a6339df80aa13bb19e6b89f6bec4167c06c2eec8
Vasyl Ishchuk <135171192+VasylIshchuk@users.noreply.github.com>
2024-04-13 23:03:12 +0200

    Part 1 (#1)
    * Task 1
    
    * Task 2
    
    * Task 3
    
    * Task 4
    
    * Task 5
    
    * Task 6
    
    * Task 7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
-public class Main {
-    public static void main(String[] args)  {
+import java.util.List;
 
+public class Main {
+    public static void main(String[] args) {
+        List<Person> peopleList = Person.fromCsv("family.csv");
+        for (Person person : peopleList) {
+            System.out.println(person + "\n");
+        }
     }
 }
