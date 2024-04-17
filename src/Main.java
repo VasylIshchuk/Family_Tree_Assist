648fe40bce5703a82a124e6675dec5cf568d57d9
VasylIshchuk <Vasyl.Ishchuk@live.umcs.edu.pl>
2024-04-17 22:07:30 +0200

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        List<Person> peopleList = Person.fromCsv("family.csv");
-        for (Person person : peopleList) {
-            System.out.println(person + "\n");
-        }
-    }
+        //List<Person> peopleList = Person.fromCsv("family.csv");
+        PlantUMLRunner.setPathJarUml("plantuml-1.2024.4.jar");
+        String data = "@startuml\n" +
+                "object JanKowalski\n" +
+                "object AnnaKowalska\n" +
+                "\n" +
+                "JanKowalski --> AnnaKowalska\n" +
+                "@enduml";
+        PlantUMLRunner.generateDiagram(data,"uml","test.txt");
+   }
 }
